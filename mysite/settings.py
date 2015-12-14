300407a5cf893eee7cc47861e94847c1a1214316
Geeksten <temozex@gmail.com>
2015-12-13 18:15:22 -1100

    fixed sttai root error

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -99,6 +100,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
